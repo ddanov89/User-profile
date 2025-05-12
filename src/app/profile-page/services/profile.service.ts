8bcb6ad34e638aba156ee0f680475432ddbcf144
ddanov89 <138501928+ddanov89@users.noreply.github.com>
2025-05-12 21:17:47 +0300

    added constants folder/updated types structure/services file updated to get all userInformation

--- src/app/profile-page/services/profile.service.ts
+++ src/app/profile-page/services/profile.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Profile } from '../types/profile.type';
+import { api } from '../../constants/api.constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+  api = api;
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getUserInformation() {
+    return this.http.get<Profile>(`${api}/users`);
+  }
 }
--- src/app/profile-page/types/profile.type.ts
+++ src/app/profile-page/types/profile.type.ts
@@ -1,5 +1,30 @@
 export interface Profile {
+  id: string;
   name: string;
+  username: string;
   email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
   avatar: File | string;
+}
+
+export interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+export interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+export interface Geo {
+  lat: string;
+  lng: string;
 }
