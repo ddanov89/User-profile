1bc80ed468ad25b9e8a3d4985751936634aee733
ddanov89 <138501928+ddanov89@users.noreply.github.com>
2025-05-14 14:58:23 +0300

    Adding routes for edit functionality and 'Not Found' page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '', pathMatch: 'full' },
-  
+
   {
     path: 'profile-page/:userId',
     loadComponent: () =>
@@ -10,4 +11,13 @@ export const routes: Routes = [
         (c) => c.ProfilePageComponent
       ),
   },
+  {
+    path: 'edit/:userId',
+    loadComponent: () =>
+      import('./edit-page/edit-page.component').then(
+        (c) => c.EditPageComponent
+      ),
+  },
+  { path: '404', component: NotFoundComponent },
+  { path: '**', redirectTo: '/404' },
 ];
