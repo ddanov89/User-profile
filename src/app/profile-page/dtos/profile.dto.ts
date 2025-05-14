40f2ab227c453eb251868289bf7cbee5df72963a
ddanov89 <138501928+ddanov89@users.noreply.github.com>
2025-05-14 16:43:00 +0300

    ProfileDTO adjustment

--- src/app/profile-page/dtos/profile.dto.ts
+++ src/app/profile-page/dtos/profile.dto.ts
@@ -2,20 +2,20 @@ export interface ProfileUpdateDTO {
   name?: string | null;
   username?: string | null;
   email?: string | null;
-  address?: Address | null;
+  address?: AddressDTO | null;
   phone?: string | null;
   website?: string | null;
-  company?: Company | null;
+  company?: CompanyDTO | null;
 //   avatar?: File | string;
 }
 
-export interface Address {
+export interface AddressDTO {
   street?: string | null;
   suite?: string | null;
   city?: string | null;
   zipcode?: string | null;
 }
 
-export interface Company {
+export interface CompanyDTO {
   name?: string | null;
 }
