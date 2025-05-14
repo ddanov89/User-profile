c891c6723382c062f197c3378e6219998c1dda8b
ddanov89 <138501928+ddanov89@users.noreply.github.com>
2025-05-14 16:40:35 +0300

    profile page implemented (html, css & ts file)

--- src/app/profile-page/profile-page.component.ts
+++ src/app/profile-page/profile-page.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ProfileService } from './services/profile.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Profile } from './types/profile.type';
+import { LoaderComponent } from '../loader/loader.component';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-profile-page',
   standalone: true,
-  imports: [],
+  imports: [LoaderComponent],
   templateUrl: './profile-page.component.html',
-  styleUrl: './profile-page.component.css'
+  styleUrl: './profile-page.component.css',
 })
-export class ProfilePageComponent {
+export class ProfilePageComponent implements OnInit, OnDestroy {
+  currentUser: Profile | null | undefined;
 
+  profileSubscription: Subscription | undefined;
+
+  constructor(
+    private profileService: ProfileService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const userId = this.route.snapshot.params['userId'];
+
+    this.profileSubscription = this.profileService
+      .getUserById(userId)
+      .subscribe({
+        next: (user) => {
+          this.currentUser = user;
+        },
+        error: (err) => {
+          console.error("Can't fetch data!");
+        },
+      });
+  }
+
+  launchEditForm() {
+    const userId = this.currentUser?.id;
+    if (userId) {
+      this.router.navigate(['/edit', userId]);
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.profileSubscription) {
+      this.profileSubscription.unsubscribe();
+    }
+  }
 }
