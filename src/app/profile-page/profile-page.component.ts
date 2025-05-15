0d2ad6518765bc2e072e6abb6357cc576c437a1d
ddanov89 <138501928+ddanov89@users.noreply.github.com>
2025-05-15 14:36:18 +0300

    cleanup

--- src/app/profile-page/profile-page.component.ts
+++ src/app/profile-page/profile-page.component.ts
@@ -1,11 +1,12 @@
-import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Profile } from './types/profile.type';
 import { map, Subscription } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { selectAllUsers } from './state/selectors/profile.selectors';
 import { loadUsers } from './state/actions/profile.actions';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { SnackBarService } from '../shared/services/snackBarService';
+import { errorMessages } from '../constants/validation-messages';
 
 @Component({
   selector: 'app-profile-page',
@@ -15,21 +16,17 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrl: './profile-page.component.css',
 })
 export class ProfilePageComponent implements OnInit, OnDestroy {
-  // private store = inject(Store);
-  // private route = inject(ActivatedRoute);
-  // private router = inject(Router);
-
   currentUser: Profile | null | undefined;
 
   defaultAvatarUrl = 'https://avatars.githubusercontent.com/u/583231?v=4';
   profileSubscription: Subscription = new Subscription();
 
-    constructor(
-      private store: Store,
-      private route: ActivatedRoute,
-      private router: Router,
-      private snackBar: MatSnackBar
-    ) {}
+  constructor(
+    private store: Store,
+    private route: ActivatedRoute,
+    private router: Router,
+    private snackBar: SnackBarService
+  ) {}
   ngOnInit(): void {
     const userId = this.route.snapshot.params['userId'];
     this.store.dispatch(loadUsers());
@@ -48,17 +45,8 @@ export class ProfilePageComponent implements OnInit, OnDestroy {
     const userId = this.currentUser?.id;
     if (userId) {
       this.router.navigate(['/edit', userId]);
-    }else{
-      this.snackBar.open(
-            "You can't perform this action now. Please try again later.",
-            'Close',
-            {
-              duration: 5000,
-              panelClass: ['snackbar-error'], 
-              verticalPosition: 'top',
-              horizontalPosition: 'center',
-            }
-          );
+    } else {
+      this.snackBar.toggleSnackBar(errorMessages.actionNotAllowed);
     }
   }
 
